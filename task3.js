487d823aa811831cedd901370fa2dff5a33f0aae
mrvigneshgaddam <vigneshgaddam100@gmail.com>
2025-01-08 11:18:39 +0530

    v0.0.3

--- task3.js
+++ task3.js
@@ -1,23 +1,28 @@
 import fs from 'fs';
 
-import ollama from "ollama";
+import  ollama  from 'ollama';
 
-async function ask_llm(q) {
+let q;
+let n=3;
+async function ask_llm(q,i) {
     const response = await ollama.chat({
         model: "llama3.1:8b",
         messages: [{ role: "user", content: q }],
     });
     let a = response.message.content;
-    fs.writeFile(`a.txt`,a,(err)=>{
+    fs.writeFile(`./Answers/a${i}.txt`,a,(err)=>{
         if(err){
             throw err;
         }
     });
 }
 
-ask_llm(fs.readFileSync("q.txt", 'utf8',(err)=>{
+for(let i=1;i<=n;i++){
+    q=`./Questions/q${i}.txt`;
+    ask_llm(fs.readFileSync(q, 'utf8',(err)=>{
         if(err){
             throw err;
         }
-    }));
-
+    }),i);
+    
+}
